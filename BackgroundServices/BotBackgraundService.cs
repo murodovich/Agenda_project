917c68f2e66a0d3c193fd79b656810cfd5c76114
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-21 18:56:47 +0500

    save

--- BackgroundServices/BotBackgraundService.cs
+++ BackgroundServices/BotBackgraundService.cs
@@ -1,11 +1,23 @@
 ﻿
+using Agenda_project.Services;
+using Telegram.Bot;
+
 namespace Agenda_project.BackgroundServices
 {
     public class BotBackgraundService : BackgroundService
     {
+        private TelegramBotClient _client;
+
+        public BotBackgraundService(TelegramBotClient client)
+        {
+            _client = client;
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Salom");
+            _client.StartReceiving<UpdateHandlerService>(null, stoppingToken);
+            return Task.CompletedTask;
         }
     }
 }
