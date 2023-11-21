9e1244435a20f50c481315509e4e902d5ddef60e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-21 18:56:55 +0500

    save

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Agenda_project.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public int ChatId { get; set; } 
+
+    }
+}
---
+++ Services/UpdateHandlerService .cs
@@ -0,0 +1,24 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Agenda_project.Services
+{
+    public class UpdateHandlerService : IUpdateHandler
+    {
+        public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+
+
+            await botClient.SendTextMessageAsync(
+                chatId: update.Message.Chat.Id,
+                text: "qalesan",
+                cancellationToken: cancellationToken);
+        }
+    }
+}
