2fea9091a69ed4922cdc7bee90c44c5451b1a969
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-22 14:51:31 +0500

    save

---
+++ Data/AgendaDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Agenda_project.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda_project.Data
+{
+    public class AgendaDBContext : DbContext
+    {
+        public AgendaDBContext(DbContextOptions<AgendaDBContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ToDos> ToDos { get; set; }
+
+        
+
+    }
+}
